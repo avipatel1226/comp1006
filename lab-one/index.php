f648df4e1670d6ead6bc06581011c7df551ca364
avipatel1226 <avipatel1226@gmail.com>
2026-01-17 15:08:22 -0500

    Update Lab Reflection comment

--- lab-one/index.php
+++ lab-one/index.php
@@ -17,9 +17,16 @@ echo "<p>" . $myCar->getInfo() . "</p>";
 /*
 Lab Reflection:
 
-I found creating the Car class and using methods straightforward. 
-Connecting to the MySQL database with PDO was a bit challenging at first, 
-especially figuring out the correct port and credentials. 
-Using include/require to organize my code was easy and helped keep the code clean.
+For this first lab, I found creating the Car class and using its methods fairly straightforward, 
+as it reinforced my understanding of classes, properties, and constructors in PHP. 
+Using include/require to organize code in separate files was easy and helped keep the project structured.
+
+The part I found more challenging was connecting to the MySQL database using PDO, 
+especially ensuring the correct port, username, and password because I already had xampp in my computer i used it in my previous
+college so it was giving me connction errors. Debugging the connection error 
+helped me understand the importance of error handling with try/catch blocks.
+
+Overall, this lab helped me practice both PHP object-oriented programming and database connectivity, 
+and it gave me confidence to work with PHP and MySQL for future projects.
 */
 ?>
